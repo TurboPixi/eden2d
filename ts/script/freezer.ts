72a527669be189ac61d3bd906fad98ddd8e013af
Joel Webber <jgw@pobox.com>
2021-04-10 11:28:15 -0400

    .

--- ts/script/freezer.ts
+++ ts/script/freezer.ts
@@ -34,7 +34,7 @@ type FreezeArr = (null|boolean|number|string)[];
 //  Lists, maps, & blocks:
 //   '[' expression* ']': list
 //   '{' (symbol, expression)* '}': dict
-//   '(' name, params, env, expr ')': block
+//   '(' name, params, env, expr ')': block -- TODO: self?
 //  Ids & Refs:
 //   '>id': set current list/map/block id (must be first item after open delimiter)
 //   '<id': ref to expression id
@@ -45,8 +45,6 @@ type FreezeArr = (null|boolean|number|string)[];
 //
 export var _freeze = [$('x'), _blk, (env: Dict) => {
   let obj = lookupSym(env, $('x'));
-
-  let curId = 1;
   let arr: FreezeArr = [];
   function freezer(obj: any): void {
     switch (typeof obj) {
@@ -88,10 +86,15 @@ export var _freeze = [$('x'), _blk, (env: Dict) => {
 
         // Only arrays and dicts get an id.
         if ('[id]' in obj) {
-          arr.push('<' + obj['[id]']);
+          let id = obj['[id]'] as number;
+          if (!('[mark]' in obj)) {
+            arr.splice(id + 1, 0, '>' + id);
+            obj['[mark]'] = true;
+          }
+          arr.push('<' + id);
           return;
         }
-        obj['[id]'] = curId++;
+        obj['[id]'] = arr.length;
 
         if (obj.constructor == Array) {
           arr.push('[');
